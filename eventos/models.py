b021cc7e52c53f2da29b32e128425fa20c13a158
Jhessica Evelyn Viana Vieira <05824355100@ifb.local>
2025-12-08 16:12:36 -0300

    Adicionando rotas de relacionamentos com GET

--- eventos/models.py
+++ eventos/models.py
@@ -1,33 +1,34 @@
 from django.db import models
 
+class Participante(models.Model):
+    nome = models.CharField(max_length=255) 
+    email = models.EmailField(max_length=255) 
+    celular = models.CharField(max_length=12)
+    tipo = models.CharField(max_length=20, choices= [('palestrante', 'Palestrante'), ('estudante', 'Estudante'), ('convidado', 'Convidado'),])
+    
+    def __str__(self):
+        return self.nome  
+
+
 class Evento(models.Model):
     nome = models.CharField(max_length=255) 
     descricao = models.CharField(max_length=500,null = True,blank = True) 
     data_inicio = models.DateField()
     data_fim = models.DateField()
     local = models.CharField(max_length=255) 
-
+    participantes = models.ManyToManyField(Participante, blank=True)
     def __str__(self):
         return self.nome 
     
-class Participante(models.Model):
-    nome = models.CharField(max_length=255) 
-    email = models.CharField(max_length=255) 
-    celular = models.CharField(max_length=12)
-    tipo = models.CharField(max_length=20, choices= [('palestrante', 'Palestrante'), ('estudante', 'Estudante'), ('convidado', 'Convidado'),])
-    evento = models.ManyToManyField(Evento)
-    
-    def __str__(self):
-        return self.nome  
-    
+
     
 class Atividade(models.Model):
     titulo = models.CharField(max_length=255) 
     descricao = models.CharField(max_length=500,null = True,blank = True) 
     hora_inicio = models.TimeField()
     hora_fim = models.TimeField()
-    eventos = models.ForeignKey(Evento,on_delete=models.CASCADE)
-    responsavel = models.ForeignKey(Participante,on_delete=models.CASCADE)
+    evento = models.ForeignKey(Evento,on_delete=models.CASCADE, related_name='atividades')
+    responsavel = models.ForeignKey(Participante,on_delete=models.CASCADE, related_name='responsavel')
     tipo = models.CharField(max_length=255, choices= [('workshop', 'Workshop'), ('palestra', 'Palestra'), ('mesa redonda', 'Mesa Redonda'), ('oficina', 'Oficina'),])
 
     def __str__(self):
