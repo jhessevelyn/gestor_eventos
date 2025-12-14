f895de1dfb8303fb3460002c6e22add38fe943cb
antoniogms <antonio.gms1998@gmail.com>
2025-12-14 12:07:31 -0300

    Adicionado enpoint dashboad

--- eventos/serializers.py
+++ eventos/serializers.py
@@ -2,32 +2,85 @@ from rest_framework import serializers
 from .models import Evento, Participante, Atividade 
 
 class ParticipanteSerializer(serializers.ModelSerializer):
+    """
+    Serializer genérico para CRUD de Participante.
+    """
     class Meta:
         model = Participante
-        fields = '__all__' 
+        fields = '__all__'
 
 class EventoSerializer(serializers.ModelSerializer):
+    """
+    Serializer genérico para CRUD de Evento.
+    """
     class Meta:
         model = Evento
         fields = '__all__'
 
 class AtividadeSerializer(serializers.ModelSerializer):
+    """
+    Serializer genérico para CRUD de Atividade.
+    """
     class Meta:
         model = Atividade
         fields = '__all__'
 
-class EventoNestedSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Evento
-        fields = ['id', 'nome', 'descricao', 'data_inicio', 'data_fim', 'local']
-
-class AtividadeNestedSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Atividade
-        fields = ['id', 'titulo', 'descricao', 'hora_inicio', 'hora_fim', 'tipo', 'responsavel']
-
 class InscricaoSerializer(serializers.Serializer):
+    """
+    Serializer para validar os dados para inscrição (POST) de um participante em um evento.
+    """
     participante = serializers.PrimaryKeyRelatedField(
         queryset = Participante.objects.all()
     )
 
+class AtividadeCreateSerializer(serializers.ModelSerializer):
+    """
+    Serializer responsável por criar atividades vinculadas a um evento
+    """
+    responsavel = serializers.PrimaryKeyRelatedField(
+        queryset = Participante.objects.all()
+    )
+
+    class Meta:
+        model = Atividade
+        fields = ['titulo', 'descricao', 'hora_inicio', 'hora_fim', 'tipo', 'responsavel']
+
+
+class AtividadeEventoSerializer(serializers.ModelSerializer):
+    """
+    Serializer responsável pela representação de atividades no contexto de um evento.
+    """
+    responsavel = ParticipanteSerializer(read_only=True)
+
+    class Meta:
+        model = Atividade
+        fields = ['id', 'titulo', 'descricao', 'hora_inicio', 'hora_fim', 'tipo', 'responsavel']
+
+
+class InscricaoSerializer(serializers.Serializer):
+    """
+    Serializer responsável por validar a inscrição de um participante em um evento.
+    """
+    participante = serializers.PrimaryKeyRelatedField(
+        queryset = Participante.objects.all()
+    )
+
+class ResponsavelSerializer(serializers.Serializer):
+    """
+    Serializer responsável por validar a atribuição ou alteração do responsável de uma atividade.
+    """
+    # campo para o PUT (escolha por id)
+    participante = serializers.PrimaryKeyRelatedField(
+        queryset=Participante.objects.all(),
+        write_only=True,
+        allow_null=True
+    )
+
+
+class EventoNestedSerializer(serializers.ModelSerializer):
+    """
+    Serializer responsável pela representação resumida de um evento (dashboard).
+    """
+    class Meta:
+        model = Evento
+        fields = ['id', 'nome', 'descricao', 'data_inicio', 'data_fim', 'local']
--- eventos/views.py
+++ eventos/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Evento, Participante, Atividade 
 from .serializers import EventoSerializer, ParticipanteSerializer, AtividadeSerializer
-from .serializers import AtividadeNestedSerializer, InscricaoSerializer
+from .serializers import EventoNestedSerializer, AtividadeEventoSerializer, InscricaoSerializer, ResponsavelSerializer, AtividadeCreateSerializer
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
  
@@ -11,52 +11,75 @@ class EventoViewSet(viewsets.ModelViewSet):
     queryset = Evento.objects.all() 
     serializer_class = EventoSerializer
 
-    @action(detail=True, methods=['get', 'post'], serializer_class=InscricaoSerializer)
+    # GET - endpoint: /eventos{id}/participantes
+    @action(detail=True, methods=['get'], serializer_class=InscricaoSerializer)
     def participantes(self, request, pk=None):
         evento = self.get_object()
-
-        if request.method == 'GET':
-            serializer = ParticipanteSerializer(evento.participantes.all(), many=True)
-            return Response(serializer.data)
-
-        if request.method == 'POST':
-            insc_serializer = InscricaoSerializer(data=request.data)
-            insc_serializer.is_valid(raise_exception=True)
-
-            participante = insc_serializer.validated_data['participante']
-
-            if evento.participantes.filter(pk=participante.pk).exists():
-                return Response(ParticipanteSerializer(participante).data, status=status.HTTP_200_OK)
-
-            evento.participantes.add(participante)
-            return Response(ParticipanteSerializer(participante).data, status=status.HTTP_201_CREATED)
-
-
-    @action(detail=True, methods=['get', 'post'], serializer_class=AtividadeNestedSerializer)
-    def atividades(self, request, pk=None):
+        serializer = ParticipanteSerializer(evento.participantes.all(), many=True)
+        return Response(serializer.data)
+    
+    # POST - endpoint: /eventos{id}/participantes
+    @participantes.mapping.post
+    def inscrever_participante(self, request, pk=None):
         evento = self.get_object()
 
-        if request.method == 'GET':
-            serializer = AtividadeNestedSerializer(evento.atividades.all(), many=True)  
-            print(serializer.data)
-            return Response(serializer.data)
+        insc_serializer = InscricaoSerializer(data=request.data)
+        insc_serializer.is_valid(raise_exception=True  )
+
+        participante = insc_serializer.validated_data['participante']
+
+        if evento.participantes.filter(pk=participante.pk).exists():
+            return Response({"detail": "Participante já inscrito neste evento."},
+                            status=status.HTTP_200_OK)
+
+        evento.participantes.add(participante)
+        return Response({"detail": "Participante inscrito com sucesso no evento."},
+                        status=status.HTTP_201_CREATED)
+
+    # GET - endpoint: /eventos/{id}/atividades
+    @action(detail=True, methods=['get'], serializer_class=AtividadeCreateSerializer)
+    def atividades(self, request, pk=None):
+        evento = self.get_object()
+        serializer = AtividadeEventoSerializer(evento.atividades.all(), many=True)  
+        return Response(serializer.data)
         
-        if request.method == 'POST':
-            data = request.data
+    # POST - enpoint: /eventos/{id}/atividades
+    @atividades.mapping.post
+    def criar_atividades(self, request, pk=None):
+        evento = self.get_object()
+        data = request.data
 
-            responsavel_id = data.get("responsavel")
-            responsavel = get_object_or_404(Participante, pk=responsavel_id)
+        responsavel_id = data.get("responsavel")
+        responsavel = get_object_or_404(Participante, pk=responsavel_id)
 
-            atividade = Atividade.objects.create(
-                evento = evento,
-                titulo = data["titulo"],
-                descricao = data.get("descricao", ""),
-                hora_inicio = data["hora_inicio"],
-                hora_fim = data["hora_fim"],
-                tipo = data["tipo"],
-                responsavel = responsavel
-            )
-            return Response(AtividadeNestedSerializer(atividade).data, status=201)
+        atividade = Atividade.objects.create(
+            evento = evento,
+            titulo = data["titulo"],
+            descricao = data.get("descricao", ""),
+            hora_inicio = data["hora_inicio"],
+            hora_fim = data["hora_fim"],
+            tipo = data["tipo"],
+            responsavel = responsavel
+        )
+        return Response(AtividadeCreateSerializer(atividade).data, status=201)
+    
+    # GET - endpoint: /eventos/{id}/dashboard
+    @action(detail=True, methods=['get'])
+    def dashboard(self, request, pk=None):
+        evento = self.get_object()
+        atividade_qs = evento.atividades.select_related('responsavel').all()
+        participantes_qs = evento.participantes.all()
+
+        evento_data = EventoNestedSerializer(evento).data
+        atividade_data = AtividadeNestedSerializer(atividade_qs, many=True).data
+        participantes_data = ParticipanteSerializer(participantes_qs, many=True).data
+
+        payload = {
+            'evento': evento_data,
+            'atividades': atividade_data,
+            'participantes': participantes_data
+        }
+        return Response(payload, status=status.HTTP_200_OK)
     
     
 class ParticipanteViewSet(viewsets.ModelViewSet):
@@ -68,10 +91,26 @@ class AtividadeViewSet(viewsets.ModelViewSet):
     queryset = Atividade.objects.all() 
     serializer_class = AtividadeSerializer
 
-    @action(detail=True, methods=['get', 'put'],serializer_class =  ParticipanteSerializer)
+    # GET - endpoint: /atividades/{id}/responsavel
+    @action(detail=True, methods=['get'], serializer_class=ResponsavelSerializer)
     def responsavel(self, request, pk=None):
         atividade = self.get_object()
-        if request.method == 'GET':
-             serializer = ParticipanteSerializer(atividade.responsavel)
-        return Response(serializer.data)
+        serializer = ParticipanteSerializer(atividade.responsavel)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
+    # POST - endpoint: /atividades/{id}/responsavel
+    @responsavel.mapping.put
+    def set_responsavel(self, request, pk=None):
+        atividade = self.get_object()
+
+        r_serializer = ResponsavelSerializer(data=request.data)
+        r_serializer.is_valid(raise_exception=True)
+        participante = r_serializer.validated_data['participante']
+
+        if atividade.responsavel and atividade.responsavel.pk == participante.pk:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        atividade.responsavel = participante
+        atividade.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
